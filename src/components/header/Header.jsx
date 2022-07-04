a20b06b1a112509ece8bb4388616a1526a32bb0a
Jarda Korinek <korinek14.j@gmail.com>
2022-07-04 23:32:43 +0200

    začátek práce na header komponentě

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./header.css";
+import CTA from "./CTA";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Ahoj, já jsem</h5>
+        <h1>Jarda Kořínek</h1>
+        <h5 className="text-light">Frontend Web Developer</h5>
+        <CTA />
+      </div>
+    </header>
+  );
 };
 
 export default Header;
