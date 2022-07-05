b4f07b21278dfe2fb94128e06e6eef23cefb5262
Jarda Korinek <korinek14.j@gmail.com>
2022-07-05 13:14:04 +0200

    přidány CTA, socialní sítě, upraven img...v Header Componente

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,10 +4,12 @@ import CV from "../../assets/cv.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href={CV} download>
+      <a href={CV} download className="btn">
         Stáhni životopis
       </a>
-      <a href="#contact">Kontaktuj mě</a>
+      <a href="#contact" className="btn btn-primary">
+        Kontaktuj mě
+      </a>
     </div>
   );
 };
