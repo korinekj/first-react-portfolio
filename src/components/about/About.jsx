5e8b0e670ca0487c2632eaadf10b57bf8d5b4880
Jarda Korinek <korinek14.j@gmail.com>
2022-07-08 21:11:00 +0200

    Finished AboutMe Section + reponsivness

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -9,11 +9,13 @@ const About = () => {
   return (
     <section id="about">
       <h5>Get To Know</h5>
-      <h1>About Me</h1>
+      <h2>About Me</h2>
 
       <div className="container about__container">
-        <div className="about__me-image">
-          <img src={AboutMeImg} alt="" />
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={AboutMeImg} alt="" />
+          </div>
         </div>
 
         <div className="about__content">
