6b234fca80a78e92d38620e6a715b7099008a640
Jarda Korinek <korinek14.j@gmail.com>
2022-07-08 17:02:01 +0200

    Finished About.JSX code

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,8 +1,56 @@
 import React from "react";
 import "./about.css";
+import AboutMeImg from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-  return <section id="about">About</section>;
+  return (
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h1>About Me</h1>
+
+      <div className="container about__container">
+        <div className="about__me-image">
+          <img src={AboutMeImg} alt="" />
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FiUsers className="about__icon" />
+              <h5>Clients</h5>
+              <small>200+ Worldwide</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>80+ Completed</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum
+            iure cumque suscipit, enim sit eos molestias labore illum mollitia
+            doloremque. A, suscipit laudantium. Numquam vero aut libero et
+            dolorem laborum.
+          </p>
+
+          <a href="#contact" className="btn btn-primary">
+            Let's talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
