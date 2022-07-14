7099d34b51dfe08b94675f2899eadd955994dbee
Jarda Korinek <korinek14.j@gmail.com>
2022-07-14 22:51:57 +0200

    CONTACT COMPONENT FINISHED + STYLING + RESPONSIVE

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,36 +12,55 @@ const Contact = () => {
       <h2>Contact Me</h2>
 
       <div className="container contact__container">
-        <div className="contact__options"></div>
-        <article className="contact__option">
-          <MdOutlineEmail />
-          <h4>Email</h4>
-          <h5>korinek14.j@gmail.com</h5>
-          <a href="mailto:korinek14.j@gmail.com" target="_blank">
-            Send a message
-          </a>
-        </article>
-        <article className="contact__option">
-          <SiMessenger />
-          <h4>Messenger</h4>
-          <h5>Jarda Kořínek</h5>
-          <a href="https://m.me/" target="_blank">
-            Send a message
-          </a>
-        </article>
-        <article className="contact__option">
-          <BsWhatsapp />
-          <h4>WhatsApp</h4>
-          <h5>korinek14.j@gmail.com</h5>
-          <a
-            href="https://api.whatsapp.com/send?phone=+420728716658"
-            target="_blank"
-          >
-            Send a message
-          </a>
-        </article>
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>korinek14.j@gmail.com</h5>
+            <a href="mailto:korinek14.j@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <SiMessenger className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Jarda Kořínek</h5>
+            <a href="https://m.me/" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+123456789</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=+420728716658"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+
         {/* END OF CONTACT OPTIONS*/}
-        <form action=""></form>
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
       </div>
     </section>
   );
