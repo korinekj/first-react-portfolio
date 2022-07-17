6529b8aee7a8ec1a91db02a946a7a7c6d754dda7
Jarda Korinek <korinek14.j@gmail.com>
2022-07-17 23:28:09 +0200

    added nav bar highlighting when scrolling the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
@@ -10,6 +10,30 @@ import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
+  //Zvýrazní aktuální sekci v Nav při scrollování stránkou
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll("nav a");
+
+    window.addEventListener("scroll", () => {
+      let current = "";
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+
+        if (window.pageYOffset >= sectionTop - 200) {
+          current = section.getAttribute("id");
+        }
+      });
+
+      navLinks.forEach((navItem) => {
+        navItem.classList.remove("active");
+        if (navItem.href === "http://localhost:3000/#" + current) {
+          navItem.classList.add("active");
+        }
+      });
+    });
+  });
   return (
     <>
       <Header />
