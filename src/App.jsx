916d524b720bad1b6042c5b5a9ee27762b66d46f
Jarda Korinek <korinek14.j@gmail.com>
2024-04-22 22:35:39 +0200

    Úpravy v záhlaví

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import React, { useEffect } from "react";
-import Header from "./components/header/Header";
-import Nav from "./components/nav/Nav";
-import About from "./components/about/About";
-import Experience from "./components/experience/Experience";
+import React, { useEffect } from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
 // import Services from "./components/services/Services";
-import Portfolio from "./components/portfolio/Portfolio";
-import Testimonials from "./components/testimonials/Testimonials";
-import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
+import Portfolio from './components/portfolio/Portfolio';
+import Testimonials from './components/testimonials/Testimonials';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 const App = () => {
   //
@@ -16,23 +16,23 @@ const App = () => {
    * class 'active' to the corresponding nav link.
    */
   const highlightNavLinkOnScroll = () => {
-    const sections = document.querySelectorAll("section");
-    const navLinks = document.querySelectorAll("nav a");
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav a');
 
-    let current = "";
+    let current = '';
 
     sections.forEach((section) => {
       const sectionTop = section.offsetTop;
 
-      if (window.pageYOffset >= sectionTop - 200) {
-        current = section.getAttribute("id");
+      if (window.scrollY >= sectionTop - 200) {
+        current = section.getAttribute('id');
       }
     });
 
     navLinks.forEach((navItem) => {
-      navItem.classList.remove("active");
-      if (navItem.href === "https://jardakorinek.cz/#" + current) {
-        navItem.classList.add("active");
+      navItem.classList.remove('active');
+      if (navItem.href === 'https://jardakorinek.cz/#' + current) {
+        navItem.classList.add('active');
       }
     });
   };
@@ -40,11 +40,11 @@ const App = () => {
   // Jako ComponentDidMount -> pouze při prvním renderu přidá EventListener ([])
   /* Adding an event listener to the window object. */
   useEffect(() => {
-    window.addEventListener("scroll", highlightNavLinkOnScroll);
+    window.addEventListener('scroll', highlightNavLinkOnScroll);
 
     /* Removing the event listener when the component unmounts. */
     return () => {
-      window.removeEventListener("scroll", highlightNavLinkOnScroll);
+      window.removeEventListener('scroll', highlightNavLinkOnScroll);
     };
   }, []);
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import "./header.css";
-import CTA from "./CTA";
-import ME from "../../assets/me.png";
-import HeaderSocials from "./HeaderSocials";
+import React from 'react';
+import './header.css';
+import CTA from './CTA';
+import ME from '../../assets/me.png';
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
     <header>
-      <div className="container header__container">
+      <div className='container header__container'>
         <h5>Ahoj, já jsem</h5>
         <h1>Jarda Kořínek</h1>
-        <h5 className="text-light">Frontend Web Developer</h5>
+        <h5 className='text-light'>Web Developer</h5>
         <CTA />
         <HeaderSocials />
 
-        <div className="me">
-          <img src={ME} alt="me" />
+        <div className='me'>
+          <img src={ME} alt='me' />
         </div>
 
-        <a href="#contact" className="scroll__down">
+        <a href='#contact' className='scroll__down'>
           Scroll Down
         </a>
       </div>
