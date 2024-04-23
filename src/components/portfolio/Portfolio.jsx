5b07eb419a78230808b5f54e52b008162a68b551
jk <korinek14.j@gmail.com>
2024-04-23 17:27:50 +0200

    Portfolio.jsx - změna textu do češtiny

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -47,7 +47,7 @@ const projects = [
 const Portfolio = () => {
   return (
     <section id="portfolio">
-      <h5>My Recent Work</h5>
+      <h5>Moje nedávná práce</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
