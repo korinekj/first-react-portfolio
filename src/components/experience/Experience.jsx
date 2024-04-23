56119102d68c00ae780e274f937ce89d5e8f0aed
jk <korinek14.j@gmail.com>
2024-04-23 17:30:14 +0200

    Experience.jsx - zmeny textu do cestiny

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,8 +5,8 @@ import { BsPatchCheckFill } from "react-icons/bs";
 const Experience = () => {
   return (
     <section id="experience">
-      <h5>What skills i Have</h5>
-      <h2>My Experience</h2>
+      <h5>Jaké dovednosti mám</h5>
+      <h2>Mé zkušenosti</h2>
 
       <div className="container experience__container">
         <div className="experience__frontend">
